79c5db0e539f31dcb1aadc59a5b36b387883fc9d
poker <48237560+DDDDDDDOU@users.noreply.github.com>
2020-11-17 23:52:05 +0800

    Update LineEdit.cpp

--- My_PCLogo/LineEdit.cpp
+++ My_PCLogo/LineEdit.cpp
@@ -6,7 +6,7 @@ LineEdit::LineEdit(QWidget* parent) : QLineEdit(parent)
     setReadOnly(true);
     this->setCursor(Qt::PointingHandCursor);
     this->setStyleSheet("QLineEdit {"
-                      "border-image:url(:/images/input.png);"
+                      "border-image:url(:/images/image/input.png);"
                       "background:rgba(0,200,128,0);"
                       "padding-left:30px;"
                       "}");
@@ -24,7 +24,7 @@ void LineEdit::keyPressEvent(QKeyEvent* e)
 void LineEdit::focusInEvent(QFocusEvent *e)
 {
     this->setStyleSheet("QLineEdit {"
-                        "border-image:url(:/images/input.png);"
+                        "border-image:url(:/images/image/input.png);"
                         "background:rgba(255,255,255,0.9);"
                         "padding-left:30px;"
                         "}");
@@ -33,7 +33,7 @@ void LineEdit::focusInEvent(QFocusEvent *e)
 void LineEdit::focusOutEvent(QFocusEvent *e)
 {
     this->setStyleSheet("QLineEdit {"
-                        "border-image:url(:/images/input.png);"
+                        "border-image:url(:/images/image/input.png);"
                         "background:rgba(0,200,128,0);"
                         "padding-left:30px;"
                         "}");
